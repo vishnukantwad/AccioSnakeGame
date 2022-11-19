ecb3ef52615e2eee05f64948b33d2bcd9239f7e8
vishnukantwad <vishnukantwad9091@gmail.com>
2022-11-19 22:58:07 +0530

    Initial Commit

---
+++ src/SnakeGame.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
+
+
+public class SnakeGame {
+    JFrame frame;
+   SnakeGame(){
+       frame=new JFrame();
+       Board board= new Board();
+       frame.add(board);
+
+       frame.setResizable(false);
+       frame.pack();
+
+
+           frame.setVisible(true);
+           frame.setBounds(100,100,400,400);
+   }
+ public static  void main(String[] args){
+       SnakeGame snakeGame=new SnakeGame();
+ }
+
+
+}
